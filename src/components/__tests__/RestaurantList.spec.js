7406513adffc610fe2a824a80b16d8bc63998668
Kristian <46797006+KristianWEB@users.noreply.github.com>
2020-04-10 21:49:35 +0300

    implemented loading and error states (#4)

--- src/components/__tests__/RestaurantList.spec.js
+++ src/components/__tests__/RestaurantList.spec.js
@@ -2,31 +2,70 @@ import React from 'react';
 import {render} from '@testing-library/react';
 import {RestaurantList} from '../RestaurantList';
 
+const restaurants = [
+  {id: 1, name: 'Sushi Place'},
+  {id: 2, name: 'Pizza Place'},
+];
+let loadRestaurants;
+let context;
+
+const renderWithProps = (propOverrides = {}) => {
+  const props = {
+    loadRestaurants: jest.fn().mockName('loadRestaurants'),
+    loading: false,
+    restaurants,
+    ...propOverrides,
+  };
+  loadRestaurants = props.loadRestaurants;
+
+  context = render(<RestaurantList {...props} />);
+};
+
 describe('RestaurantList', () => {
-  const restaurants = [
-    {id: 1, name: 'Sushi Place'},
-    {id: 2, name: 'Pizza Place'},
-  ];
-  let loadRestaurants;
-  let context;
-
-  beforeEach(() => {
-    loadRestaurants = jest.fn().mockName('loadRestaurants');
-
-    context = render(
-      <RestaurantList
-        loadRestaurants={loadRestaurants}
-        restaurants={restaurants}
-      />,
-    );
-  });
   it('loads restaurants on first render', () => {
+    renderWithProps();
     expect(loadRestaurants).toHaveBeenCalled();
   });
+});
+
+describe('while loading', () => {
+  it('displays the loading indicator while loading', () => {
+    renderWithProps({loading: true});
+    const {queryByTestId} = context;
+    expect(queryByTestId('loading-indicator')).not.toBeNull();
+  });
+});
+
+describe('when loading succeeds', () => {
+  beforeEach(() => {
+    renderWithProps();
+  });
+
+  it('does not display the loading indicator while not loading', () => {
+    const {queryByTestId} = context;
+    expect(queryByTestId('loading-indicator')).toBeNull();
+  });
+
   it('displays the restaurants', () => {
     const {queryByText} = context;
 
     expect(queryByText('Sushi Place')).not.toBeNull();
     expect(queryByText('Pizza Place')).not.toBeNull();
+  });
+
+  it('does not display the error message', () => {
+    const {queryByText} = context;
+    expect(queryByText('Restaurants could not be loaded.')).toBeNull();
+  });
+});
+
+describe('when loading fails', () => {
+  beforeEach(() => {
+    renderWithProps({loadError: true});
+  });
+
+  it('displays the error message', () => {
+    const {queryByText} = context;
+    expect(queryByText('Restaurants could not be loaded.')).not.toBeNull();
   });
 });
--- src/store/__tests__/restaurants.spec.js
+++ src/store/__tests__/restaurants.spec.js
@@ -3,28 +3,110 @@ import thunk from 'redux-thunk';
 import restaurantsReducer from '../restaurants/reducers';
 import {loadRestaurants} from '../restaurants/actions';
 
-describe('restaurants', () => {
-  describe('loadRestaurants action', () => {
-    it('stores the restaurants', async () => {
-      const records = [
-        {id: 1, name: 'Sushi Place'},
-        {id: 2, name: 'Pizza Place'},
-      ];
-      const api = {
-        loadRestaurants: () => Promise.resolve(records),
-      };
-      const initialState = {
-        records: [],
-      };
-      const store = createStore(
-        restaurantsReducer,
-        initialState,
-        applyMiddleware(thunk.withExtraArgument(api)),
-      );
+const records = [
+  {id: 1, name: 'Sushi Place'},
+  {id: 2, name: 'Pizza Place'},
+];
 
-      await store.dispatch(loadRestaurants());
+describe('when loading succeeds', () => {
+  let store;
+  beforeEach(() => {
+    const api = {
+      loadRestaurants: () => Promise.resolve(records),
+    };
 
-      expect(store.getState().records).toEqual(records);
-    });
+    const initialState = {
+      records: [],
+    };
+
+    store = createStore(
+      restaurantsReducer,
+      initialState,
+      applyMiddleware(thunk.withExtraArgument(api)),
+    );
+
+    return store.dispatch(loadRestaurants());
+  });
+  it('stores the restaurants', () => {
+    expect(store.getState().records).toEqual(records);
+  });
+  it('clears the loading flag', () => {
+    expect(store.getState().loading).toEqual(false);
+  });
+});
+describe('while loading', () => {
+  let store;
+
+  beforeEach(() => {
+    const api = {
+      loadRestaurants: () => new Promise(() => {}),
+    };
+
+    const initialState = {loadError: true};
+
+    store = createStore(
+      restaurantsReducer,
+      initialState,
+      applyMiddleware(thunk.withExtraArgument(api)),
+    );
+
+    store.dispatch(loadRestaurants());
+  });
+
+  it('sets a loading flag', () => {
+    expect(store.getState().loading).toEqual(true);
+  });
+
+  it('clears the error flag', () => {
+    expect(store.getState().loadError).toEqual(false);
+  });
+});
+
+describe('initially', () => {
+  let store;
+
+  beforeEach(() => {
+    const initialState = {};
+
+    store = createStore(
+      restaurantsReducer,
+      initialState,
+      applyMiddleware(thunk),
+    );
+  });
+
+  it('does not have the loading flag set', () => {
+    expect(store.getState().loading).toEqual(false);
+  });
+  it('does not have the error flag set', () => {
+    expect(store.getState().loadError).toEqual(false);
+  });
+});
+
+describe('when loading fails', () => {
+  let store;
+
+  beforeEach(() => {
+    const api = {
+      loadRestaurants: () => Promise.reject(),
+    };
+
+    const initialState = {};
+
+    store = createStore(
+      restaurantsReducer,
+      initialState,
+      applyMiddleware(thunk.withExtraArgument(api)),
+    );
+
+    return store.dispatch(loadRestaurants());
+  });
+
+  it('sets an error flag', () => {
+    expect(store.getState().loadError).toEqual(true);
+  });
+
+  it('clears the loading flag', () => {
+    expect(store.getState().loading).toEqual(false);
   });
 });
