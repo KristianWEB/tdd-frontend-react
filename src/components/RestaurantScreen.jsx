10fbe98efb5afc3a7f0acd3d83aadb88da00b28c
Kristian <devWEBpk@outlook.com>
2020-04-10 18:16:00 +0300

    styling with material design done

--- src/components/RestaurantScreen.jsx
+++ src/components/RestaurantScreen.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 import RestaurantList from './RestaurantList';
 
 const RestaurantScreen = () => (
-  <div>
-    <h1>Restaurants</h1>
-    <RestaurantList />
-  </div>
+  <Card>
+    <CardContent>
+      <Typography variant="h5">Restaurants</Typography>
+      <RestaurantList />
+    </CardContent>
+  </Card>
 );
 
 export default RestaurantScreen;
